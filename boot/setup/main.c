f8a2c9f64f1a16fbba39c9eecf1c8bf4081b94da
Xingang Wang <wxg.xingang@outlook.com>
2023-07-02 19:40:03 +0800

    boot: format code style
    Signed-off-by: Xingang Wang <wxg.xingang@outlook.com>

--- boot/setup/main.c
+++ boot/setup/main.c
@@ -39,28 +39,28 @@
 /* waitdisk - wait for disk ready */
 static void waitdisk(void)
 {
-    while ((inb(0x1F7) & 0xC0) != 0x40)
-        /* do nothing */;
+	while ((inb(0x1F7) & 0xC0) != 0x40)
+		/* do nothing */;
 }
 
 /* readsect - read a single sector at @secno into @dst */
 static void readsect(void *dst, uint32_t secno)
 {
-    // wait for disk to be ready
-    waitdisk();
+	// wait for disk to be ready
+	waitdisk();
 
-    outb(0x1F2, 1); // count = 1
-    outb(0x1F3, secno & 0xFF);
-    outb(0x1F4, (secno >> 8) & 0xFF);
-    outb(0x1F5, (secno >> 16) & 0xFF);
-    outb(0x1F6, ((secno >> 24) & 0xF) | 0xE0);
-    outb(0x1F7, 0x20); // cmd 0x20 - read sectors
+	outb(0x1F2, 1); // count = 1
+	outb(0x1F3, secno & 0xFF);
+	outb(0x1F4, (secno >> 8) & 0xFF);
+	outb(0x1F5, (secno >> 16) & 0xFF);
+	outb(0x1F6, ((secno >> 24) & 0xF) | 0xE0);
+	outb(0x1F7, 0x20); // cmd 0x20 - read sectors
 
-    // wait for disk to be ready
-    waitdisk();
+	// wait for disk to be ready
+	waitdisk();
 
-    // read a sector
-    insl(0x1F0, dst, SECTSIZE / 4);
+	// read a sector
+	insl(0x1F0, dst, SECTSIZE / 4);
 }
 
 /* *
@@ -69,52 +69,52 @@ static void readsect(void *dst, uint32_t secno)
  * */
 static void readseg(uintptr_t va, uint32_t count, uint32_t offset)
 {
-    uintptr_t end_va = va + count;
+	uintptr_t end_va = va + count;
 
-    // round down to sector boundary
-    va -= offset % SECTSIZE;
+	// round down to sector boundary
+	va -= offset % SECTSIZE;
 
-    // translate from bytes to sectors; kernel starts at sector 1
-    uint32_t secno = (offset / SECTSIZE) + 1;
+	// translate from bytes to sectors; kernel starts at sector 1
+	uint32_t secno = (offset / SECTSIZE) + 1;
 
-    // If this is too slow, we could read lots of sectors at a time.
-    // We'd write more to memory than asked, but it doesn't matter --
-    // we load in increasing order.
-    for (; va < end_va; va += SECTSIZE, secno++) {
-        readsect((void *)va, secno);
-    }
+	// If this is too slow, we could read lots of sectors at a time.
+	// We'd write more to memory than asked, but it doesn't matter --
+	// we load in increasing order.
+	for (; va < end_va; va += SECTSIZE, secno++) {
+		readsect((void *)va, secno);
+	}
 }
 
 /* bootmain - the entry of bootloader */
 void bootmain(void)
 {
-    // read the 1st page off disk
-    readseg((uintptr_t)ELFHDR, SECTSIZE * 8, SECTSIZE * 2);
+	// read the 1st page off disk
+	readseg((uintptr_t)ELFHDR, SECTSIZE * 8, SECTSIZE * 2);
 
-    // is this a valid ELF?
-    if (ELFHDR->e_magic != ELF_MAGIC) {
-        goto bad;
-    }
+	// is this a valid ELF?
+	if (ELFHDR->e_magic != ELF_MAGIC) {
+		goto bad;
+	}
 
-    struct proghdr *ph, *eph;
+	struct proghdr *ph, *eph;
 
-    // load each program segment (ignores ph flags)
-    ph = (struct proghdr *)((uintptr_t)ELFHDR + ELFHDR->e_phoff);
-    eph = ph + ELFHDR->e_phnum;
-    for (; ph < eph; ph++) {
-        readseg(ph->p_va & 0xFFFFFF, ph->p_memsz, SECTSIZE * 2 + ph->p_offset);
-    }
+	// load each program segment (ignores ph flags)
+	ph = (struct proghdr *)((uintptr_t)ELFHDR + ELFHDR->e_phoff);
+	eph = ph + ELFHDR->e_phnum;
+	for (; ph < eph; ph++) {
+		readseg(ph->p_va & 0xFFFFFF, ph->p_memsz, SECTSIZE * 2 + ph->p_offset);
+	}
 
-    readseg((uintptr_t)RES_ADDR, SECTSIZE * (8 + 116), SECTSIZE * 8000);
+	readseg((uintptr_t)RES_ADDR, SECTSIZE * (8 + 116), SECTSIZE * 8000);
 
-    // call the entry point from the ELF header
-    // note: does not return
-    ((void (*)(void))(ELFHDR->e_entry & 0xFFFFFF))();
+	// call the entry point from the ELF header
+	// note: does not return
+	((void (*)(void))(ELFHDR->e_entry & 0xFFFFFF))();
 
 bad:
-    outw(0x8A00, 0x8A00);
-    outw(0x8A00, 0x8E00);
+	outw(0x8A00, 0x8A00);
+	outw(0x8A00, 0x8E00);
 
-    /* do nothing */
-    //    while (1);
+	/* do nothing */
+	//    while (1);
 }
